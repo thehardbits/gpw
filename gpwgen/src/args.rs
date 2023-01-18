ec4938151b5ea8ff3b8dd436f07be86a04ce1e37
Jay Kickliter <jay@kickliter.com>
2023-01-18 13:22:34 -0700

    Implement combine subcommand

--- gpwgen/src/args.rs
+++ gpwgen/src/args.rs
@@ -1,14 +1,35 @@
 use clap::Parser;
 
-/// Process global world population (GPW) asc files into serialized H3
+#[derive(Parser, Debug)]
+pub enum Args {
+    Tessellate(Tessellate),
+    Combine(Combine),
+}
+
+/// Tessellate global world population (GPW) asc file grids into H3
 /// cell/value pairs.
 #[derive(Parser, Debug)]
-pub struct Args {
+pub struct Tessellate {
     /// Intermediate H3 resolution.
     #[arg(short, long, default_value_t = 10)]
-    pub res: u8,
+    pub resolution: u8,
     /// Input GPW ASCII file.
-    pub src: std::path::PathBuf,
+    pub sources: Vec<std::path::PathBuf>,
     /// Output directory.
-    pub out: std::path::PathBuf,
+    #[arg(short, long)]
+    pub outdir: std::path::PathBuf,
+}
+
+/// Combine multiple h3tess files into a single serialized H3 map at
+/// the specified resolution.
+#[derive(Parser, Debug)]
+pub struct Combine {
+    /// H3 resolution.
+    #[arg(short, long, default_value_t = 8)]
+    pub resolution: u8,
+    /// h3tess source files.
+    pub sources: Vec<std::path::PathBuf>,
+    /// Output file.
+    #[arg(short, long)]
+    pub output: std::path::PathBuf,
 }
--- gpwgen/src/main.rs
+++ gpwgen/src/main.rs
@@ -1,25 +1,128 @@
+use anyhow::{anyhow, Result};
+use byteorder::{LittleEndian as LE, ReadBytesExt, WriteBytesExt};
 use clap::Parser;
-use gpwgen::{args::Args, generate::gen_to_disk, gpwascii::GpwAscii};
+use gpwgen::{
+    args::{Args, Combine, Tessellate},
+    generate::gen_to_disk,
+    gpwascii::GpwAscii,
+};
+use hextree::{
+    compaction::Compactor,
+    h3ron::{FromH3Index, H3Cell},
+    HexTreeMap,
+};
 use std::{
     fs::File,
-    io::{BufReader, BufWriter},
+    io::{BufReader, BufWriter, ErrorKind},
     path::PathBuf,
 };
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    let mut rdr = BufReader::new(File::open(&args.src).unwrap());
-    let data = GpwAscii::parse(&mut rdr).unwrap();
-
-    // Create the path to the output file with H3 resolution added and
-    // gpwh3 extension.
-    let dst_path = {
-        let mut dst = PathBuf::new();
-        dst.push(args.out);
-        dst.push(args.src.file_name().unwrap());
-        dst.set_extension(format!("res{}.h3tess", args.res));
-        dst
+    match args {
+        Args::Tessellate(tess_args) => tessellate(tess_args)?,
+        Args::Combine(combine_args) => combine(combine_args)?,
     };
-    let mut dst = BufWriter::new(File::create(dst_path).unwrap());
-    gen_to_disk(data, &mut dst);
+    Ok(())
+}
+
+fn tessellate(
+    Tessellate {
+        resolution,
+        sources,
+        outdir,
+    }: Tessellate,
+) -> Result<()> {
+    // Open all source and destination files at the same time,
+    // otherwise fail fast.
+    let files = sources
+        .iter()
+        .map(|src_path| -> Result<(File, File)> {
+            let src_file = File::open(src_path)?;
+
+            // Create the path to the output file with H3 resolution added and
+            // gpwh3 extension.
+            let dst_path = {
+                let src_filename = src_path
+                    .file_name()
+                    .ok_or_else(|| anyhow!(format!("Not a file {:?}", src_path)))?;
+                let mut dst = PathBuf::new();
+                dst.push(&outdir);
+                dst.push(src_filename);
+                dst.set_extension(format!("res{}.h3tess", resolution));
+                dst
+            };
+            let dst_file = File::create(dst_path)?;
+            Ok((src_file, dst_file))
+        })
+        .collect::<Result<Vec<(File, File)>>>()?;
+
+    for (src_file, dst_file) in files {
+        let mut rdr = BufReader::new(src_file);
+        let mut dst = BufWriter::new(dst_file);
+        let data = GpwAscii::parse(&mut rdr).unwrap();
+        gen_to_disk(data, &mut dst)
+    }
+
+    Ok(())
+}
+
+fn combine(
+    Combine {
+        resolution,
+        sources,
+        output,
+    }: Combine,
+) -> Result<()> {
+    // Open all source files at the same time, otherwise fail fast.
+    let sources = sources
+        .iter()
+        .map(File::open)
+        .collect::<std::io::Result<Vec<File>>>()?;
+    let output_file = File::create(output)?;
+
+    let mut map: HexTreeMap<f32, _> = HexTreeMap::with_compactor(SummationCompactor { resolution });
+
+    for source in sources {
+        let mut rdr = BufReader::new(source);
+        loop {
+            match (rdr.read_u64::<LE>(), rdr.read_f32::<LE>()) {
+                (Ok(h3_index), Ok(val)) => {
+                    let cell = H3Cell::from_h3index(h3_index);
+                    map.insert(cell, val)
+                }
+                (Err(e), _) if e.kind() == ErrorKind::UnexpectedEof => break,
+                (err @ Err(_), _) => {
+                    err?;
+                }
+                (_, err @ Err(_)) => {
+                    err?;
+                }
+            };
+        }
+    }
+
+    let mut wtr = BufWriter::new(output_file);
+    for (cell, val) in map.iter() {
+        wtr.write_u64::<LE>(**cell)?;
+        wtr.write_f32::<LE>(*val)?;
+    }
+
+    Ok(())
+}
+
+struct SummationCompactor {
+    resolution: u8,
+}
+
+impl Compactor<f32> for SummationCompactor {
+    fn compact(&mut self, res: u8, children: [Option<&f32>; 7]) -> Option<f32> {
+        if res < self.resolution {
+            return None;
+        }
+        if let [Some(v0), Some(v1), Some(v2), Some(v3), Some(v4), Some(v5), Some(v6)] = children {
+            return Some(v0 + v1 + v2 + v3 + v4 + v5 + v6);
+        };
+        None
+    }
 }
