d20c12ca7c7c49c21ff83c038d42dc76868ad673
Jay Kickliter <jay@kickliter.com>
2023-01-18 18:18:55 -0700

    [server] add hexmap deserialization progress bar

--- gpws/src/main.rs
+++ gpws/src/main.rs
@@ -10,17 +10,23 @@ use hyper::{
     service::{make_service_fn, service_fn},
     Error, Response, Server, StatusCode,
 };
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use std::{
     convert::TryFrom,
     fs::File,
-    io::{BufReader, ErrorKind, Read},
-    sync::Arc,
+    io::{BufReader, ErrorKind},
+    sync::{
+        atomic::{AtomicBool, AtomicU64, Ordering},
+        Arc,
+    },
+    thread,
+    time::Duration,
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = options::Cli::parse();
-    let f = BufReader::new(File::open(args.path)?);
+    let f = File::open(args.path)?;
     let map: HexTreeMap<f32> = deserialize_hexmap(f)?;
     let map = Arc::new(map);
 
@@ -58,22 +64,68 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-fn deserialize_hexmap(mut rdr: impl Read) -> Result<HexTreeMap<f32>> {
+fn deserialize_hexmap(src_file: File) -> Result<HexTreeMap<f32>> {
+    let file_size = src_file.metadata()?.len();
     let mut map = HexTreeMap::new();
-    loop {
-        match (rdr.read_u64::<LE>(), rdr.read_f32::<LE>()) {
-            (Ok(h3_index), Ok(val)) => {
-                let cell = H3Cell::try_from(h3_index)?;
-                map.insert(cell, val)
-            }
-            (Err(e), _) if e.kind() == ErrorKind::UnexpectedEof => break,
-            (err @ Err(_), _) => {
-                err?;
-            }
-            (_, err @ Err(_)) => {
-                err?;
-            }
-        };
+    let mut ret_err: Option<anyhow::Error> = None;
+    let idx_val_pairs_total =
+        file_size / (std::mem::size_of::<u64>() + std::mem::size_of::<f32>()) as u64;
+    let idx_val_pairs_processed = AtomicU64::new(0);
+
+    {
+        println!("Deserializing hex map");
     }
-    Ok(map)
+
+    let hexmap_complete = AtomicBool::new(false);
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            let mut rdr = BufReader::new(src_file);
+            loop {
+                match (rdr.read_u64::<LE>(), rdr.read_f32::<LE>()) {
+                    (Ok(h3_index), Ok(val)) => {
+                        let cell = H3Cell::try_from(h3_index)
+                            .expect("serialized hexmap should only contain valid indices");
+                        map.insert(cell, val);
+                        idx_val_pairs_processed.fetch_add(1, Ordering::Relaxed);
+                    }
+                    (Err(e), _) if e.kind() == ErrorKind::UnexpectedEof => {
+                        hexmap_complete.store(true, Ordering::Relaxed);
+                        break;
+                    }
+                    (Err(e), _) => {
+                        ret_err = Some(e.into());
+                        hexmap_complete.store(true, Ordering::Relaxed);
+                        break;
+                    }
+                    (_, Err(e)) => {
+                        ret_err = Some(e.into());
+                        hexmap_complete.store(true, Ordering::Relaxed);
+                        break;
+                    }
+                };
+            }
+        });
+
+        s.spawn(|| {
+            let pb = ProgressBar::new(idx_val_pairs_total);
+            pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {human_pos}/{human_len} ({eta})")
+                         .expect("incorrect progress bar format string")
+                         .with_key("eta", |state: &ProgressState, w: &mut dyn std::fmt::Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+                         .progress_chars("#>-"));
+
+            while !hexmap_complete.load(Ordering::Relaxed) && idx_val_pairs_processed.load(Ordering::Relaxed) < idx_val_pairs_total {
+                pb.set_position(idx_val_pairs_processed.load(Ordering::Relaxed));
+                thread::sleep(Duration::from_millis(12));
+            }
+
+            pb.finish_with_message("done");
+        });
+    });
+
+    if let Some(err) = ret_err {
+        Err(err)
+    } else {
+        Ok(map)
+    }
 }
