0d58907b4fe443babdd1210ef4dbf0b86cfa80d8
Jay Kickliter <jay@kickliter.com>
2023-01-20 10:36:57 -0700

    Use jemalloc

--- gpws/src/main.rs
+++ gpws/src/main.rs
@@ -22,6 +22,12 @@ use std::{
     thread,
     time::Duration,
 };
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
